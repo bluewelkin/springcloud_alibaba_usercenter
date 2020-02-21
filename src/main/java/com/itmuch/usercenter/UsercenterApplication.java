c616d65d71fc828155c5fcb06e8aae73e7852004
bluewelkin <denggx@126.com>
2020-02-21 14:11:51 +0800

    init1.0 可以运行成功

---
+++ src/main/java/com/itmuch/usercenter/UsercenterApplication.java
@@ -0,0 +1,14 @@
+package com.itmuch.usercenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan("com.itmuch")
+public class UsercenterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UsercenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itmuch/springcloud_alibaba_usercenter/SpringcloudAlibabaUsercenterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.itmuch.springcloud_alibaba_usercenter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringcloudAlibabaUsercenterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
