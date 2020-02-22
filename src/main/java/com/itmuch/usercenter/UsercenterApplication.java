aca6d73ebdc085cabc065d60f94f2ea1479c8375
bluewelkin <denggx@126.com>
2020-02-22 21:02:05 +0800

    用这个版本就能服务注册，晕

--- src/main/java/com/itmuch/usercenter/UsercenterApplication.java
+++ src/main/java/com/itmuch/usercenter/UsercenterApplication.java
@@ -2,13 +2,25 @@ package com.itmuch.usercenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("com.itmuch")
+
 public class UsercenterApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(UsercenterApplication.class, args);
     }
 
+    @RestController
+    public class EchoController {
+        @GetMapping(value = "/echo/{string}")
+        public String echo(@PathVariable String string) {
+            return "Hello Nacos Discovery " + string;
+        }
+    }
 }
